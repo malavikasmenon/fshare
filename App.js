13c94207d3519e39aa694746aa166ff4fee98acc
Rose Joseph <rosejosephpmrs@gmail.com>
2022-05-13 07:50:19 +0530

    Revert "Revert "added chat-screen""
    This reverts commit 34a4fcce6909b35ea1122bde4a5649f2676fef77.

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import {View, StyleSheet} from 'react-native';
 
-export default function App() {
+import Chat from './screens/Chat';
+
+import {
+  Octicons,
+  MaterialCommunityIcons,
+} from '@expo/vector-icons';
+import ChatScreen from './screens/ChatScreen';
+import FileSendIcon from './components/FileSendIcon';
+
+const Stack = createStackNavigator();
+
+function ChatStack() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen name='FShare' component={ChatScreen} options={{title:"FShare", headerRight: () => (
+            <View style={{
+              flexDirection: 'row',
+              width: 60,
+              justifyContent: 'space-between',
+              marginRight: 10,
+            }}>
+              <Octicons name="search" size={22} color={'black'} />
+              <MaterialCommunityIcons name="dots-vertical" size={22} color={'black'} />
+            </View>
+          )}} />
+        <Stack.Screen name="Chat" component={Chat} options={({ route })  => ({
+          title: route.params.name,
+          headerRight: () => (
+            <View style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              marginRight: 10
+            }}>
+              
+              {/*<MaterialCommunityIcons name="file-send-outline" size={22} color="black" style={styles.icon}/>*/}
+              <FileSendIcon size={22} iconstyles={styles.icon} />
+              <MaterialCommunityIcons name="dots-vertical" size={22} color={'black'} style={{padding:8}}/>
+            </View>
+          )
+        })}/>
+    </Stack.Navigator>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  icon: {
+  padding:8, backgroundColor: '#BEBEBE', borderRadius:22, opacity:0.7
+  }
 });
+
+function RootNavigator() {
+  return (
+    <NavigationContainer>
+      <ChatStack />
+    </NavigationContainer>
+  );
+}
+
+export default function App() {
+  return <RootNavigator />;
+}
---
+++ screens/Chat.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { GiftedChat } from 'react-native-gifted-chat'
+import { View } from 'react-native-web'
+import { StyleSheet } from 'react-native-web';
+import { AntDesign } from '@expo/vector-icons';
+
+export default function Chat() {
+  return (
+    
+    <GiftedChat />
+    
+  )
+}
+
